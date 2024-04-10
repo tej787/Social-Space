f90cd91b48d3841f9cec0832a43f051083d9b83e
Tej Tandel <tejtandel2809@gmail.com>
2024-04-11 02:17:58 +0530

    Done

--- SocialMedia-Backend/Controller/errorController.js
+++ SocialMedia-Backend/Controller/errorController.js
@@ -25,26 +25,19 @@ const handleJWTExpiredError = () =>
 
 const sendErrorDev = (err, req,res) => {
 
-  if (req.originalUrl.startsWith('/api')) {
+  
     return res.status(err.statusCode).json({
       status: err.status,
       error: err,
       message: err.message,
       stack: err.stack
     });
-  }
-
-  // B) RENDERED WEBSITE
-  console.error('ERROR 💥', err);
-  return res.status(err.statusCode).render('error', {
-    title: 'Something went wrong!',
-    msg: err.message
-  });
+  
 };
 
 const sendErrorProd = (err,req,  res) => {
 // A) API
-if (req.originalUrl.startsWith('/api')) {
+
   // A) Operational, trusted error: send message to client
   if (err.isOperational) {
     return res.status(err.statusCode).json({
@@ -60,24 +53,8 @@ if (req.originalUrl.startsWith('/api')) {
     status: 'error',
     message: 'Something went very wrong!'
   });
-}
 
-// B) RENDERED WEBSITE
-// A) Operational, trusted error: send message to client
-if (err.isOperational) {
-  return res.status(err.statusCode).render('error', {
-    title: 'Something went wrong!',
-    msg: err.message
-  });
-}
-// B) Programming or other unknown error: don't leak error details
-// 1) Log error
-console.error('ERROR 💥', err);
-// 2) Send generic message
-return res.status(err.statusCode).render('error', {
-  title: 'Something went wrong!',
-  msg: 'Please try again later.'
-});
+
 };
 
 module.exports = (err, req, res, next) => {
