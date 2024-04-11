4c2b34b5e36829aee5b957034239acb7bfe530d8
Tej Tandel <tejtandel2809@gmail.com>
2024-04-11 12:37:58 +0530

    Done

--- SocialMedia-Backend/Controller/authController.js
+++ SocialMedia-Backend/Controller/authController.js
@@ -23,7 +23,7 @@ const signToken = id => {
       expires: new Date(
         Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
       ),
-      httpOnly: true,
+      httpOnly: false,
       secure: false,
       sameSite: 'none'
     });
