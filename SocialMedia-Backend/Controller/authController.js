859acfa17514f4f926d0e21f925dcc823e725770
Tej Tandel <tejtandel2809@gmail.com>
2024-04-11 12:27:33 +0530

    Done

--- SocialMedia-Backend/Controller/authController.js
+++ SocialMedia-Backend/Controller/authController.js
@@ -24,8 +24,8 @@ const signToken = id => {
         Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
       ),
       httpOnly: true,
-      secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
-      sameSite: 'lax'
+      secure: false,
+      sameSite: 'none'
     });
 
     
